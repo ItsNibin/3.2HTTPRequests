dee37f93cd618e7656b7d9df4f5151b1eefec272
Nibin <nibinmathias@gmail.com>
2025-01-26 11:56:02 +0530

    added the get requests

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+app.get('/', (req, res) => {
+  //console.log(req.rawHeaders);
+  res.send("<h1>Hello Worlds!</h1>");
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+app.get('/contact', (req, res) => {
+  //console.log(req.rawHeaders);
+  res.send("Email me at: nibinmathias10@gmail.com");
 });
 
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+app.get('/about', (req, res) => {
+  //console.log(req.rawHeaders);
+  res.send("I am a software engineer at Wipro Technologies");
 });
 
 app.listen(port, () => {
